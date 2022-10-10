91fbbc111278d752f91b3e1857378a494acb4587
Abbes Mohamed <abbesmohamed717@gmail.com>
2022-10-10 18:49:38 +0100

    download plugins on jenkins server through a script

--- setup/jenkins-plugins/installer.sh
+++ setup/jenkins-plugins/installer.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
+#SPECIFY THE RIGHT CREDENTIALS OF THE USER THAT WOULD ACCESS THE SERVER AND INSTALL THE PLUGINS
+JENKINS_URL='http://192.168.0.0:8080'
 
-set -eo pipefail
+JENKINS_CRUMB=$(curl -s --cookie-jar /tmp/cookies -u abbes:140063 ${JENKINS_URL}/crumbIssuer/api/json | jq .crumb -r)
 
-JENKINS_URL='http://localhost:8080'
-
-JENKINS_CRUMB=$(curl -s --cookie-jar /tmp/cookies -u admin:admin ${JENKINS_URL}/crumbIssuer/api/json | jq .crumb -r)
-
-JENKINS_TOKEN=$(curl -s -X POST -H "Jenkins-Crumb:${JENKINS_CRUMB}" --cookie /tmp/cookies "${JENKINS_URL}/me/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken?newTokenName=demo-token66" -u admin:admin | jq .data.tokenValue -r)
+JENKINS_TOKEN=$(curl -s -X POST -H "Jenkins-Crumb:${JENKINS_CRUMB}" --cookie /tmp/cookies "${JENKINS_URL}/me/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken?newTokenName=demo-token66" -u abbes:140063 | jq .data.tokenValue -r)
 
 echo $JENKINS_URL
 echo $JENKINS_CRUMB
@@ -14,7 +12,7 @@ echo $JENKINS_TOKEN
 
 while read plugin; do
    echo "........Installing ${plugin} .."
-   curl -s POST --data "<jenkins><install plugin='${plugin}' /></jenkins>" -H 'Content-Type: text/xml' "$JENKINS_URL/pluginManager/installNecessaryPlugins" --user "admin:$JENKINS_TOKEN"
+   curl -s POST --data "<jenkins><install plugin='${plugin}' /></jenkins>" -H 'Content-Type: text/xml' "$JENKINS_URL/pluginManager/installNecessaryPlugins" --user "abbes:$JENKINS_TOKEN"
 done < plugins.txt
 
 
